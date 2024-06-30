6beb429b590699ef8ac3aa3839dce681e5e61114
YL羽 <574300833@qq.com>
2024-06-30 16:36:59 +0800

    Update RestTemplateConfig.java

--- dcloud-common/src/main/java/com/ysl/config/RestTemplateConfig.java
+++ dcloud-common/src/main/java/com/ysl/config/RestTemplateConfig.java
@@ -1,25 +1,62 @@
 package com.ysl.config;
 
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+
     @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory requestFactory){
+    public RestTemplate restTemplate(ClientHttpRequestFactory requestFactory) {
         return new RestTemplate(requestFactory);
     }
 
+    @Bean
+    public ClientHttpRequestFactory httpRequestFactory() {
+        return new HttpComponentsClientHttpRequestFactory(httpClient());
+    }
 
     @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
-        SimpleClientHttpRequestFactory factory  = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(10000);
-        factory.setConnectTimeout(10000);
-        return factory;
+    public HttpClient httpClient() {
+
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+
+        connectionManager.setMaxTotal(500);
+        connectionManager.setDefaultMaxPerRoute(300);
+
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setSocketTimeout(20000)
+                .setConnectTimeout(10000)
+                .setConnectionRequestTimeout(1000)
+                .build();
+
+        CloseableHttpClient closeableHttpClient = HttpClientBuilder
+                .create()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(connectionManager)
+                .build();
+
+        return closeableHttpClient;
     }
+
 }
