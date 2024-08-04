cd8264c445091cedf898efd99e60cea548d9584e
YL羽 <574300833@qq.com>
2024-08-04 22:21:14 +0800

    update register

--- dcloud-account/src/main/java/com/ysl/controller/AccountController.java
+++ dcloud-account/src/main/java/com/ysl/controller/AccountController.java
@@ -1,12 +1,15 @@
 package com.ysl.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ysl.controller.request.AccountRegisterRequest;
 import com.ysl.enums.BizCodeEnum;
+import com.ysl.service.AccountService;
 import com.ysl.service.FileService;
 import com.ysl.util.JsonData;
 
@@ -20,9 +23,11 @@ import com.ysl.util.JsonData;
 public class AccountController {
 
     private FileService fileService;
+    private AccountService accountService;
 
-    AccountController(FileService fileService) {
+    AccountController(FileService fileService, AccountService accountService) {
         this.fileService = fileService;
+        this.accountService = accountService;
     }
 
     @PostMapping("upload")
@@ -33,5 +38,11 @@ public class AccountController {
         return result != null ? JsonData.buildSuccess(result)
                 : JsonData.buildResult(BizCodeEnum.FILE_UPLOAD_USER_IMG_FAIL);
 
+    }
+
+    @PostMapping("register")
+    public JsonData register(@RequestBody AccountRegisterRequest registerRequest) {
+        JsonData jsonData = accountService.register(registerRequest);
+        return jsonData;
     }
 }
--- dcloud-account/src/main/java/com/ysl/manager/impl/AccountManagerImpl.java
+++ dcloud-account/src/main/java/com/ysl/manager/impl/AccountManagerImpl.java
@@ -1,11 +1,26 @@
 package com.ysl.manager.impl;
 
 import com.ysl.manager.AccountManager;
+import com.ysl.mapper.AccountMapper;
+import com.ysl.model.AccountDO;
+
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class AccountManagerImpl implements AccountManager {
-    
+    private AccountMapper accountMapper;
+
+    AccountManagerImpl(AccountMapper accountMapper) {
+        this.accountMapper = accountMapper;
+    }
+
+    @Override
+    public int insert(AccountDO accountDO) {
+
+        return accountMapper.insert(accountDO);
+
+    }
+
 }
